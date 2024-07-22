67bc213cf7368c0c4e2465f1c4f6b770682d335f
raziyeaydin <raziye.aydin@bil.omu.edu.tr>
2024-07-21 21:18:12 -0700

    006 Divisible Sum Pairs challenge is completed

---
+++ src/main/java/org/example/VI_divisibleSumPairs/Main.java
@@ -0,0 +1,56 @@
+package org.example.VI_divisibleSumPairs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int result = divisibleSumPairs(n, k, ar);
+        bufferedReader.close();
+    }
+
+    /*
+     *  Target Method
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+    public static int divisibleSumPairs(int n, int k, List<Integer> intList) {
+        int result = 0;
+
+        for (int i = 0; i < intList.size() - 1; i++) {
+            for (int j = i + 1; j < intList.size(); j++) {
+                int subSum = Integer.sum(intList.get(i), intList.get(j));
+                if (Integer.valueOf(0).equals(subSum % k)) {
+                    System.out.println("(" + i + "," + j + ") --> " + intList.get(i) + "+" + intList.get(j) + "=" + (intList.get(i) + intList.get(j)));
+                    ++result;
+                }
+            }
+        }
+        System.out.println(result);
+        return result;
+
+    }
+
+}
